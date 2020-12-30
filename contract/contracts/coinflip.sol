b7a298e9b62a88c7c58b3f252d9217d3b83791ca
rkindle <ralf.kindle@gmail.com>
2020-12-30 19:49:30 +0100

    Contract code clean up and comments; Remove obsolete code

--- contract/contracts/coinflip.sol
+++ contract/contracts/coinflip.sol
@@ -36,6 +36,7 @@ contract Coinflip is Ownable, usingProvable{
   uint constant NUM_RANDOM_BYTES_REQUESTED = 1;
 
   function createPlayer(address _creator, uint _betValue) private{
+    //Function to creat a player initially
     Player memory newPlayer;
     newPlayer.id = 1;
     newPlayer.queryId = 0;
@@ -55,11 +56,13 @@ contract Coinflip is Ownable, usingProvable{
   }
 
   function insertPlayer(Player memory _newPlayer) private{
+    //Function to add new player
     address creator = msg.sender;
     player[creator] = _newPlayer;
   }
 
   function getPlayer() public view returns(uint lastRandomNumber, uint lastResult, uint lastBetValue, uint lastWin, uint totalWin, uint totalPlay, uint totalWon, uint lastWinPayed, uint unpayedWinnings){
+    //Function to get the player stats
     address creator = msg.sender;
     return (player[creator].lastRandomNumber, player[creator].lastResult, player[creator].lastBetValue,
       player[creator].lastWin, player[creator].totalWin, player[creator].totalPlay, player[creator].totalWon,
@@ -67,11 +70,14 @@ contract Coinflip is Ownable, usingProvable{
   }
 
   function updatePlayer(address _creator, uint _lastResult, uint _lastWin) private {
+    //Function to update player stats and information
     player[_creator].lastResult = _lastResult;
     player[_creator].lastWin = _lastWin;
     player[_creator].totalWin += _lastResult;
     player[_creator].totalPlay += 1;
     player[_creator].totalWon += _lastWin;
+    //Check for unpayed winnigs and accordingly add winings or just overwrite
+    //May be obsolete due to recent change to updatePlayerPayout() function
     if (player[_creator].lastWinPayed == 0){
       player[_creator].unpayedWinnings += 2*_lastWin;
     }
@@ -82,6 +88,7 @@ contract Coinflip is Ownable, usingProvable{
   }
 
   function updatePlayerPayout(address _creator, uint _lastWinPayed) private{
+    //Function to ensure payout is only possible when payouts are pending
     player[_creator].lastWinPayed = _lastWinPayed;
     if (_lastWinPayed == 1){
       player[_creator].unpayedWinnings = 0;
@@ -89,6 +96,7 @@ contract Coinflip is Ownable, usingProvable{
   }
 
   function updatePlayerQuery(address _creator, bytes32 _queryId, uint _randomNumber) private {
+    //Function to change oracel query status and update player
     queries[_queryId] = _creator;
     player[_creator].queryId = _queryId;
     player[_creator].lastRandomNumber = _randomNumber;
@@ -96,13 +104,14 @@ contract Coinflip is Ownable, usingProvable{
   }
 
   function updateBetValue(address _creator, uint _betValue) private{
+    //Function to change only the bet Value - maybe not necessary
     player[_creator].lastBetValue = _betValue;
-    emit currentBetValue(_betValue+1);
+    emit currentBetValue(_betValue);
   }
 
 
   function playerNotExists(address _creator) private view returns (bool exists){
-    //address creator = msg.sender;
+    //Check if player already exists in the listing
     if (player[_creator].id != 0){
       exists = false;
     }
@@ -112,12 +121,8 @@ contract Coinflip is Ownable, usingProvable{
     return exists;
   }
 
-  /*function random() private view returns(uint result){
-    uint res = block.timestamp % 2;
-    return res;
-  }*/
-
   function __callback(bytes32 _queryId, string memory _result, bytes memory _proof) public {
+    //Funciton oracel callback
     require(msg.sender == provable_cbAddress());
     address creator = queries[_queryId];
 
@@ -135,6 +140,7 @@ contract Coinflip is Ownable, usingProvable{
   }
 
   function oracleRandom() private {
+    //Function for the oracle call
     address creator = msg.sender;
     uint QUERY_EXECUTION_DELAY = 0;
     uint GAS_FOR_CALLBACK = 2000000;
@@ -146,12 +152,12 @@ contract Coinflip is Ownable, usingProvable{
 
 
   function coinFlip() payable public{
+    //Main Funciton to flip a coin
     uint bet_value = msg.value;
     address creator = msg.sender;
-    //uint winnings;
-    //uint winner;
-    require(msg.value <= address(this).balance, "Insufficient contract balance");
-    require(player[creator].queryId == 0, "Current result pending");
+
+    require(msg.value <= address(this).balance, "Insufficient contract balance"); //Needs to be changed to consider the unpaid winnings
+    require(player[creator].queryId == 0, "Current result pending"); //Prevents new bet whilst a query is still pending
 
     if (playerNotExists(creator)){
       createPlayer(creator, bet_value);
@@ -160,55 +166,34 @@ contract Coinflip is Ownable, usingProvable{
       updateBetValue(creator, bet_value);
     }
 
-    emit currentBetValue(bet_value);
-
     oracleRandom();
-
-    //uint res = random();
-    //uint res = block.timestamp % 2;
-    /*if(res == 0){
-      winner = 0;
-      balance = balance + bet_value;
-      winnings = 0;
-      emit coinflipResult(res, winnings);
-    }
-    else if(res == 1){
-      winner = 1;
-      winnings = 2* bet_value;
-      balance = balance - bet_value;
-      msg.sender.transfer(winnings);
-      emit coinflipResult(res, winnings);
-    }
-    else{
-      emit uncoughtException(1);
-    }
-    updatePlayer(winner, winnings);
-    return (winner, winnings);
-    */
   }
 
   function payout() public payable returns(uint){
+    //Function to payout the unpayed winnings
     address payable payCreator = msg.sender;
     require(player[payCreator].lastWinPayed == 0, "No payout available");
 
-    //uint res = player[payCreator].lastRandomNumber;
     emit newPayoutInitiated("Payout initiated");
 
     payCreator.transfer(player[payCreator].unpayedWinnings);
 
-    updatePlayerPayout(payCreator, 1);
+    updatePlayerPayout(payCreator, 1);  //Set the payout to has been executed
     emit payedOut(player[payCreator].unpayedWinnings);
 
-    return player[payCreator].unpayedWinnings;
+    return player[payCreator].unpayedWinnings;  //Will return 0 due to the last change on updatePlayerPayout - add variable to carry value
   }
 
   function depositeBalance() public onlyOwner payable returns(uint) {
+    //Function to add balance to the contract
       uint newBalance = address(this).balance + msg.value;
       balance = newBalance;
       return newBalance;
   }
 
   function withdrawAll() public onlyOwner payable returns(uint) {
+    //Function to withdraw entire contract balance
+    //Change Required: Consider the unpayed winnings of the players for withdrawable balance
       uint toTransfer = address(this).balance;
       balance = 0;
       msg.sender.transfer(toTransfer);
@@ -217,6 +202,8 @@ contract Coinflip is Ownable, usingProvable{
   }
 
   function withdrawAmount(uint _amount) public onlyOwner payable returns(uint) {
+    //Function to withdraw an amount to be specified
+    //Change Required: Consider the upayed winnings of the players for withdrawable balance
       require(_amount <= address(this).balance, "Requested amount exeeds current balance");
       balance = address(this).balance - _amount;
       msg.sender.transfer(_amount);
@@ -225,10 +212,12 @@ contract Coinflip is Ownable, usingProvable{
   }
 
   function getBalance() public view returns(uint){
+    //Function to request contract balance
     return address(this).balance;
   }
 
   function isQueryPending() public view returns(uint){
+    //Function to check if a oracle query is waiting for callback
     address creator = msg.sender;
     if(player[creator].queryId == 0){
       return 0;
